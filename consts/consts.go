850fdc62040fd1c16d01d293bd467726c01ff2a5
komoere <mildredkomoere@gmail.com>
2024-07-23 12:15:30 +0000

    komoere

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "mild"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "red"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ko"
 )
 
 var ID ids.ID
